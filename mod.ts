a43646bea58bf51973489c4ec2fff4fd5896b53b
Shahid N. Shah <shahid@shah.org>
2020-07-08 18:06:31 -0400

    initial commit after extraction from IGS Specification Commons

---
+++ mod.ts
@@ -0,0 +1,218 @@
+export interface InterpolationOptions {
+  bracketPrefixes?: string[];
+  openBracket: string;
+  closeBracket: string;
+}
+
+export interface InterpolationExprOptions extends InterpolationOptions {
+  pathDelims: string[];
+}
+
+export interface InterpolationSupplier {
+  (replace: string, bracketPrefix?: string): string;
+}
+
+export interface InterpolatePropFetcherParams {
+  readonly options: InterpolationOptions;
+  readonly findExpr: string;
+  readonly bracketPrefix?: string;
+  readonly lookInObj: any;
+  readonly level: number;
+  readonly parent?: any;
+  readonly parentDelim?: string;
+  readonly remaining?: string;
+}
+
+export interface InterpolatePropFetcherResult {
+  readonly params: InterpolatePropFetcherParams;
+  readonly foundExpr: boolean;
+  readonly foundObj: any;
+}
+
+export interface InterpolatePropFetcher {
+  (params: InterpolatePropFetcherParams): InterpolatePropFetcherResult;
+}
+
+export interface InterpolateProperty {
+  (params: InterpolatePropFetcherResult): string;
+}
+
+export interface InterpolatePropertyForPrefix {
+  readonly prefixedBracket: boolean;
+  readonly bracketPrefixes?: [string];
+  readonly handler: InterpolateProperty;
+}
+
+export function defaultPropFetcher(
+  params: InterpolatePropFetcherParams,
+): InterpolatePropFetcherResult {
+  return {
+    params: params,
+    foundExpr: params.findExpr in params.lookInObj,
+    foundObj: params.lookInObj[params.findExpr],
+  };
+}
+
+export function interpolationPropSupplier(
+  lookInObject: object | object[],
+  options: InterpolationExprOptions,
+  fetcher: InterpolatePropFetcher,
+  foundHandler: InterpolateProperty | InterpolatePropertyForPrefix[],
+  notFoundHandler?: InterpolationSupplier,
+): InterpolationSupplier {
+  return (replace: string, bracketPrefix?: string): string => {
+    const inObjs = Array.isArray(lookInObject) ? lookInObject : [lookInObject];
+    for (const obj of inObjs) {
+      const fetched = fetchFromObject(
+        options,
+        fetcher,
+        obj,
+        replace,
+        bracketPrefix,
+      );
+      if (fetched.foundExpr) {
+        if (Array.isArray(foundHandler)) {
+          for (const ipfp of foundHandler) {
+            if (
+              (ipfp.prefixedBracket === false &&
+                typeof bracketPrefix === "undefined") ||
+              (ipfp.prefixedBracket &&
+                ipfp.bracketPrefixes?.filter((bp) => bp == bracketPrefix))
+            ) {
+              return ipfp.handler(fetched);
+            }
+          }
+        } else {
+          return foundHandler(fetched);
+        }
+      }
+    }
+    return notFoundHandler
+      ? notFoundHandler(replace, bracketPrefix)
+      : `${
+        bracketPrefix ? bracketPrefix : ""
+      }${options.openBracket}${replace}${options.closeBracket}`;
+  };
+}
+
+function fetchFromObject(
+  options: InterpolationExprOptions,
+  fetcher: InterpolatePropFetcher,
+  lookInObj: any,
+  findExpr: string,
+  bracketPrefix?: string,
+  level: number = 0,
+  parent?: any,
+  parentDelim?: string,
+): InterpolatePropFetcherResult {
+  const params = {
+    findExpr,
+    bracketPrefix,
+    lookInObj,
+    options,
+    level,
+    parent,
+    parentDelim,
+  };
+  if (typeof lookInObj === "undefined") {
+    return { params: params, foundExpr: false, foundObj: undefined };
+  }
+
+  let pathDelimIdx = -1;
+  let matchedDelim = undefined;
+  for (const delim of options.pathDelims) {
+    pathDelimIdx = findExpr.indexOf(delim);
+    if (pathDelimIdx > -1) {
+      matchedDelim = delim;
+      break;
+    }
+  }
+  if (matchedDelim && pathDelimIdx > -1) {
+    const newExpr = findExpr.substring(0, pathDelimIdx);
+    const remaining = findExpr.substr(pathDelimIdx + matchedDelim.length);
+    const fetched = fetcher({
+      ...params,
+      findExpr: newExpr,
+    });
+    if (!fetched) {
+      return {
+        params: params,
+        foundExpr: false,
+        foundObj: undefined,
+      };
+    }
+    return fetchFromObject(
+      options,
+      fetcher,
+      fetched.foundObj,
+      remaining,
+      bracketPrefix,
+      level + 1,
+      parent,
+      matchedDelim,
+    );
+  }
+  return fetcher(params);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function interpolate(
+  template: string,
+  replsSupplier: InterpolationSupplier,
+  options: InterpolationOptions,
+): string {
+  const tmplLen = template.length;
+  const out = [];
+  const acc = [];
+  const capture = [];
+  let prefix = undefined;
+  let openned = false;
+  let i = 0;
+  while (i < tmplLen) {
+    if (!openned) {
+      acc.push(template[i]);
+    } else {
+      capture.push(template[i]);
+    }
+
+    if (!openned && acc.length > 1) {
+      out.push(acc.shift());
+    }
+
+    const j = acc.join("");
+    if (!openned && j === options.openBracket) {
+      openned = true;
+      acc.length = 0;
+      if (i > 1 && options.bracketPrefixes) {
+        for (const bp of options.bracketPrefixes) {
+          if (template[i - 1] === bp) {
+            prefix = bp;
+            out.length = out.length - 1;
+          }
+        }
+      }
+    } else if (
+      openned &&
+      capture
+          .slice(
+            capture.length - 1,
+            capture.length,
+          )
+          .join("") === options.closeBracket
+    ) {
+      openned = false;
+      const toRender = capture
+        .slice(0, capture.length - 1)
+        .join("")
+        .trim();
+      out.push(replsSupplier(toRender, prefix));
+      prefix = undefined;
+      capture.length = 0;
+    }
+    i++;
+  }
+  if (acc.length !== 0) {
+    out.push(acc.join(""));
+  }
+  return out.join("");
+}
---
+++ mod_test.ts
@@ -0,0 +1,115 @@
+import * as ws from "https://cdn.jsdelivr.net/gh/shah/text-whitespace/mod.ts";
+import { assertEquals } from "https://deno.land/std@v0.60.0/testing/asserts.ts";
+import * as interp from "./mod.ts";
+
+Deno.test("Text interpolation with string supplier", () => {
+  const goldenRenderSingleLine =
+    `select col1, col2, col3# from table where col1 = 'Value with spaces' and %col2 = "Another set of spaces"`;
+  const testTemplate = `
+    select {a}, {b}, #{c}
+      from {entity}
+     where {a} = 'Value with spaces' 
+       and %{b} = "Another set of spaces"`;
+
+  const interpolationOptions: interp.InterpolationOptions = {
+    bracketPrefixes: ["#", "%"],
+    openBracket: "{",
+    closeBracket: "}",
+  };
+  assertEquals(
+    goldenRenderSingleLine,
+    ws.singleLineTrim(
+      interp.interpolate(
+        testTemplate,
+        (src: string, bracketPrefix?: string): string => {
+          switch (src) {
+            case "a":
+              return "col1";
+            case "b":
+              return bracketPrefix ? bracketPrefix + "col2" : "col2";
+            case "c":
+              return "col3" + bracketPrefix;
+            case "entity":
+              return "table";
+          }
+          return "#ERROR#";
+        },
+        interpolationOptions,
+      ),
+    ),
+  );
+});
+
+Deno.test("Text interpolation with object supplier", () => {
+  const goldenRenderSingleLine =
+    `select col1, col2, col3 from table where table.col1 = 'Value with spaces' and table.col2 = "Another set of spaces"`;
+  const testTemplate = `
+    select {entity:a}, {entity:b}, {entity::c}
+      from {entity}
+     where {entity.a} = 'Value with spaces' 
+       and {entity.b} = "Another set of spaces"`;
+  const exprSupplierObj = {
+    entity: {
+      isEntity: true,
+      tableName: "table",
+      attrs: [
+        { isAttr: true, attrName: "a", colName: "col1" },
+        { isAttr: true, attrName: "b", colName: "col2" },
+        { isAttr: true, attrName: "c", colName: "col3" },
+      ],
+    },
+  };
+  const fetcher = (
+    params: interp.InterpolatePropFetcherParams,
+  ): interp.InterpolatePropFetcherResult => {
+    if (params.level == 0 && params.findExpr == "entity") {
+      return {
+        params: params,
+        foundExpr: true,
+        foundObj: params.lookInObj.entity,
+      };
+    }
+    if (params.lookInObj.attrs) {
+      for (const a of params.lookInObj.attrs) {
+        if (params.findExpr == a.attrName) {
+          return { params: params, foundExpr: true, foundObj: a };
+        }
+      }
+    }
+    return { params: params, foundExpr: false, foundObj: undefined };
+  };
+
+  const interpolationOptions: interp.InterpolationExprOptions = {
+    openBracket: "{",
+    pathDelims: [".", "::", ":"],
+    closeBracket: "}",
+  };
+  assertEquals(
+    goldenRenderSingleLine,
+    ws.singleLineTrim(
+      interp.interpolate(
+        testTemplate,
+        interp.interpolationPropSupplier(
+          exprSupplierObj,
+          interpolationOptions,
+          fetcher,
+          (
+            { params, foundExpr: found, foundObj }:
+              interp.InterpolatePropFetcherResult,
+          ): string => {
+            if (found) {
+              if ("isEntity" in foundObj) return foundObj.tableName;
+              if ("isAttr" in foundObj) {
+                return params.parentDelim == "."
+                  ? `table.${foundObj.colName}`
+                  : foundObj.colName;
+              }
+            }
+            return `${params.findExpr}?`;
+          },
+        ),
+        interpolationOptions,
+      ),
+    ),
+  );
+});
